0bb5c7bf2eeb52d1f84c77d5b5c8d1be2049ec0c
Skill Template <template@skill>
2026-05-09 23:21:54 +0800

    CRITICAL FIX: restore session on load so all features work after login

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import { useAuthStore } from './stores/authStore'
+import { useThemeStore } from './stores/themeStore'
+
+// Init auth and theme on app mount
+function AppWithInit() {
+  useEffect(() => {
+    useAuthStore.getState().checkStoredSession()
+    useThemeStore.getState().init()
+  }, [])
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppWithInit />
     </BrowserRouter>
   </StrictMode>,
 )
